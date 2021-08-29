ba43b522a0e13b7ed73d49ff46677480fdfefae6
Daniel Botea <danielmbotea@gmail.com>
2021-08-29 17:53:55 +0300

    add pages

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function Aboutpage() {
+  return (
+      <div>
+        <h1>About</h1>
+        <p>This is an app to find the latest DJ an other musical events</p>
+      </div>
+  );
+};
+
