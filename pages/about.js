d8e017eeaba53051d101f753a2ec36f85e590ebe
Daniel Botea <danielmbotea@gmail.com>
2021-09-03 00:16:41 +0300

    add pages 2

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
-export default function Aboutpage() {
+import Link from 'next/link';
+import Layout from "../components/Layout";
+
+export default function AboutPage() {
   return (
-      <div>
+      <Layout title='About Dj events'>
         <h1>About</h1>
         <p>This is an app to find the latest DJ an other musical events</p>
-      </div>
+        <Link href="/">Home</Link>
+      </Layout>
   );
 };
 
